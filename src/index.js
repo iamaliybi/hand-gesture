97069b1b59ac3f0e404876c47a06b1ca35e9adfa
Edward Joseph <dev.edwardjoseph@gmail.com>
2021-12-22 18:37:02 +0330

    DOM elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert('Gesture')
+import * as tfjs from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+
+// DOM
+import dom, { createEl } from './dom';
+
+// Styles
+import './assets/scss/app.scss';
+
+/* Variables */
+let wrapper;
+
+/* Development */
+const init = () => {
+	wrapper = dom();
+	load();
+}
+
+document.addEventListener('DOMContentLoaded', init);
